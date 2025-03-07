580fee08957750859709cab963891484888ab69a
lapshuntanya <lapshun.tatiana@gmail.com>
2025-03-07 17:22:56 +0200

    nextDay, prevDay

--- Date.cpp
+++ Date.cpp
@@ -1,1 +1,102 @@
 #include "Date.h"
+
+Date::Date(){
+    auto sec = time(0);
+    tm* obj = new tm;
+    localtime_s(obj, &sec);
+   // obj = localtime(&sec); XCode
+
+    day = obj->tm_mday;
+    month = obj->tm_mon + 1;
+    year = obj->tm_year + 1900;
+    delete obj;
+}
+
+Date::Date(int year, int month, int day){
+    this->year = year;
+    this->month = month;
+    this->day = day;
+}
+
+bool Date::isLeapYear() const{
+    return year % 4 == 0 && year % 100 != 0 || year % 100 == 0 && year % 400 == 0;
+}
+
+int Date::monthDays() const{
+    switch (month)
+    {
+    case 1:  case 3:  case 5:  case 7:  case 8:  case 10:  case 12:
+        return 31;
+    case 2:
+        return isLeapYear() ? 29 : 28;
+    case 4: case 6: case 9: case 11:
+        return 30;
+    }
+    return 0;
+}
+
+void Date::nextDate(){
+    if (day == 31 && month == 12) {
+        day = 1;
+        month = 1;
+        year++;
+    }
+    else if (day == monthDays()) {
+        month++;
+        day = 1;
+    }
+    else {
+        day++;
+    }
+}
+
+void Date::prevDate(){
+    if (day == 1 && month == 1) {
+        day = 31;
+        month = 12;
+        year--;
+    }
+    else if (day == 1) {
+        month--;
+        day = monthDays();
+    }
+    else {
+        day--;
+    }
+}
+
+void Date::setYear(int year)
+{
+}
+
+int Date::getYear() const
+{
+    return 0;
+}
+
+void Date::setMonth(int month)
+{
+}
+
+int Date::getMonth() const
+{
+    return 0;
+}
+
+void Date::setDay(int day)
+{
+}
+
+int Date::getDay() const
+{
+    return 0;
+}
+
+void Date::showDate() const
+{
+}
+
+bool Date::valid() const
+{
+    return false;
+}
